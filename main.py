62aab4b0c50b7de75a0cdd52dfa9af02518cf7e9
GlitchedMango <98410716+GlitchedMango@users.noreply.github.com>
2024-11-18 23:18:57 +0100

    Rich basics

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
 from random import randint 
 from numpy import array, compress
-from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
+from rich import padding, print, box
+from rich.console import Console
 import os
 
-console = Console
+console = Console(style="") 
 
 class SlotMachine: 
     def __init__(self) -> None: 
-        self.test_array = array([[randint(1, 3), randint(1, 3), randint(1, 3)],
-                        [randint(1, 3), randint(1, 3), randint(1, 3)],
-                        [randint(1, 3), randint(1, 3), randint(1, 3)]])
-
-        self.lines = [{"name": "horizontal_line_0", "value": compress([True, False, False], self.test_array, axis=0)},
-             {"name": "horizontal_line_1", "value": compress([False, True, False], self.test_array, axis=0)},
-             {"name": "horizontal_line_2", "value": compress([False, False, True], self.test_array, axis=0)}]
+        self.lines = [{"name": "hor_line_1", "value": [randint(1, 3), randint(1, 3), randint(1, 3)]},
+             {"name": "hor_line_2", "value": [randint(1, 3), randint(1, 3), randint(1, 3)]},
+             {"name": "hor_line_3", "value": [randint(1, 3), randint(1, 3), randint(1, 3)]}]
+        self.clear = lambda:os.system("cls")
     
     def check_lines(self):
-
         for line in self.lines:
-            if all(x == line["value"][0][0] for x in line["value"][0]):
+            if all(x == line["value"][0] for x in line["value"]):
                 print(line["name"])
-        print(self.test_array)
 
+    def start_game(self):
+        self.check_lines()
+        self.draw_screen()
+
+    def draw_screen(self):
+        slot_table = Table(style="yellow", show_header=False)
+        slot_table.add_row(str(self.lines[-1]["value"][0]), str(self.lines[0]["value"][1]), str(self.lines[0]["value"][2]))
+        slot_table.add_row(str(self.lines[0]["value"][0]), str(self.lines[1]["value"][1]), str(self.lines[1]["value"][2]))
+        slot_table.add_row(str(self.lines[1]["value"][0]), str(self.lines[2]["value"][1]), str(self.lines[2]["value"][2]))
+
+        gui_grid = Table.grid(expand=True)
+        gui_grid.add_row(Panel(":snake: Python Slot Machine :snake:"))
+        gui_grid.add_row(Panel(slot_table, expand=True))
+
+        console.print(gui_grid, style="yellow", justify="center")
 
 if __name__ == "__main__":
     slotmachine = SlotMachine()
+    slotmachine.start_game()
     slotmachine.check_lines()
